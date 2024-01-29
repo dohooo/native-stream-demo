cad9f7045e5e8ff6b81247c5f61e0a16c05b2fa5
Caspian <zhaodonghao586@outlook.com>
2024-01-29 14:08:43 +0000

    Initial Commit

---
+++ stream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stream
+//
+//  Created by Caspian Zhao on 2024/1/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stream/streamApp.swift
@@ -0,0 +1,17 @@
+//
+//  streamApp.swift
+//  stream
+//
+//  Created by Caspian Zhao on 2024/1/29.
+//
+
+import SwiftUI
+
+@main
+struct streamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
